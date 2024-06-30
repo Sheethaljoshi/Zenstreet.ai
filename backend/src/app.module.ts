bf23c88d0c7147bc163279b7b9c16c7debb3221b
Sheethal <sh33thal24@gmail.com>
2024-07-01 03:24:24 +0530

    configured user authentication backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common"
+import { MongooseModule } from "@nestjs/mongoose"
+import { AppController } from "./app.controller"
+import { AppService } from "./app.service"
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot("mongodb+srv://sh33thal24:VFc2ef7jWrNISO6O@cluster0.8nhtj2n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
